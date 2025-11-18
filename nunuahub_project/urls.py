86673e23e3d0b4869072d883602e697ee532ab13
Brian Omoto <brian.omoto@strathmore.edu>
2025-11-18 12:40:25 +0300

    Complete project update: Add comprehensive README, fix image display, resolve merge conflicts, and update team documentation

--- nunuahub_project/urls.py
+++ nunuahub_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include  
 from django.http import HttpResponse
@@ -15,3 +17,5 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
